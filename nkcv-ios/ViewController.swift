11d46d56f454da350687693c01f0b1d04961f16d
kbinani <kbinani.bt@gmail.com>
2019-02-08 02:08:21 +0900

    init

---
+++ nkcv-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nkcv-ios
+//
+//  Created by kbinani on 2019/02/08.
+//  Copyright © 2019 kbinani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
